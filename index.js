7dfbcbd97730c905130b919bc21b232220da2b3f
bdonys <brandonsur@yahoo.com>
2024-01-06 22:55:53 -0800

    added inquirer questions

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+// Function for three character limit text
+
+
+
+// Function for color keyword OR hexadecimal code
+
+
+
+// Function for shapes
+
+
+// Function to initialize app
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Please enter up to three characters for your text: ',
+            },
+            {
+                type: 'input',
+                name: 'color',
+                message: 'Please enter a color keyword or hexadecimal code: ',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Please choose a shape: ',
+                choices: ['circle', 'square', 'triangle'],
+            }
+        ])
+}
