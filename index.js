e1d8baa635fc84db8aa8fdc34fa6206b0afb323f
bdonys <brandonsur@yahoo.com>
2024-01-07 21:45:37 -0800

    revised exported code to function with inquirer questions

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// Create textColor and shapeColor
-// Export textColor and shapeColor
-
-
-
-
 
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { createLogo } = require('./lib/createLogo');
+const { validateColor, createLogo } = require('./lib/createLogo');
+
 
 // Function for three character limit text
 const text = (input) => {
@@ -18,46 +13,53 @@ const text = (input) => {
     return true;
 };
 
-// Function for color keyword OR hexadecimal code
-
-
-
 // Function to initialize app
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter up to three characters for your text: ',
+        validate: text,
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter a basic color keyword or hexadecimal code for the text: ',
+        validate: validateColor,
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Please enter a basic color keyword or hexadecimal code for the shape: ',
+        validate: validateColor,
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Please choose a shape: ',
+        choices: ['circle', 'square', 'triangle'],
+    }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Successfully created logo!');
+        }
+    })
+}
+
 function init() {
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'Please enter up to three characters for your text: ',
-                validate: text,
-            },
-            {
-                type: 'input',
-                name: 'textColor',
-                message: 'Please enter a color keyword or hexadecimal code for the text: ',
-            },
-            {
-                type: 'input',
-                name: 'shapeColor',
-                message: 'Please enter a color keyword or hexadecimal code for the shape: ',
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                message: 'Please choose a shape: ',
-                choices: ['circle', 'square', 'triangle'],
-            }
-        ])
+        .prompt(questions)
         .then((answers) => {
-            const svgContent = createLogo(answers);
-            fs.writeFileSync('logo.svg', svgContent);
-            console.log('Generated logo.svg');
+            const logo = createLogo(answers);
+            writeToFile('logo.svg', logo);
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+};
+
 
 // Function call to initialize app
 init();
